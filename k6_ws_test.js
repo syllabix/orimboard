8bc1f26a887319634aa7a3f3783e958d8d83648f
Bhavani Morarjee <bhavani@miro.com>
2022-11-07 18:41:56 +0100

    K6 - random user generation with draw action

--- k6_ws_test.js
+++ k6_ws_test.js
@@ -4,60 +4,77 @@ import ws from 'k6/ws';
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
-const sessionDuration = 30000; // user session 30s
+const sessionDuration = 15000; // user session 15s
 
 export const options = {
-  // TODO: Add more virtual users
-  vus: 1,
-  iterations: 1,
+  vus: 3,
+  duration: 2000,
   summaryTrendStats: ["med", "min", "max", "avg", "p(90)", "p(95)", "p(99)", "p(99.9)", "p(100)"],
 };
 
-//FIXME - Need to write an action that can be deserialized properly into a valid board message; join is a simple one
-// TODO - replace user id every time server is restarted
-const payload = " \n" +
-    "action\n" +
-    ": \n" +
-    "{\"type\": \"join\", payload: {id: 46664, name: \"333\", color: \"#e86361\"}}";
+const payload = JSON.stringify({
+    "type": "draw",
+    "payload": {
+        "id": "1667842046146",
+        "point": {
+            "x": randomIntBetween(100,900),
+            "y": randomIntBetween(10,1000)
+        },
+        "color": "#34ebc0",
+        "action": "stroke"
+    }
+});
+
 export default function () {
+    const user_params = {
+        headers: { 'Content-Type': 'application/json' }
+    };
 
-  const jar = http.cookieJar();
-  // TODO: Will be nice to get each virtual user (VU) their own user ID
-  // TODO: Change URL
-  jar.set("http://localhost:8080/v1/board/90210/connect", "token", "46664");
+    // Generate a random user
+    const user_body = JSON.stringify({
+        "name": randomString(5),
+    });
+    let result_user = http.put('http://localhost:8081/v1/user', user_body, user_params);
+    console.log(result_user.body);
 
-  const url = 'ws://localhost:8080/v1/board/90210/connect';
-  const params = { tags: { my_tag: 'system-team-test session' } };
+    //TODO: Try with multiple boards! Uncomment when remote URL
+    //const random_board_id = randomIntBetween(1, 1000)
+    const random_board_id = 90210
 
-  const res = ws.connect(url, params, function (socket) {
-    socket.on('open', function open() {
-      console.log(`VU ${__VU}: connected`);
+    // Watch automated drawing!
+    const url = 'ws://localhost:8080/v1/board/' + random_board_id + '/connect?tk=' + result_user.json().id;
 
-      socket.send(JSON.stringify({ event: 'SET_NAME', new_name: 'Croc ${__VU}' }));
+    const params = {tags: {my_tag: 'system-team-test session'}};
 
-      socket.setInterval(function timeout() {
-        socket.send(payload);
-      }, 1000 ); // send something every 1 second
+    const res = ws.connect(url, params, function (socket) {
+      socket.on('open', function open() {
+        console.log(`VU ${__VU}: connected`);
+
+        socket.send(JSON.stringify({event: 'SET_NAME', new_name: 'Croc ${__VU}'}));
+
+        socket.setInterval(function timeout() {
+          socket.send(payload);
+        }, 1000); // send something every 1 second
+      });
+
+      socket.on('ping', function () {
+        console.log('PING!');
+      });
+
+      socket.on('pong', function () {
+        console.log('PONG!');
+      });
+
+      socket.on('close', function () {
+        console.log(`VU ${__VU}: disconnected`);
+      });
+
+      socket.setTimeout(function () {
+        console.log('Closing the socket forcefully 3s after graceful LEAVE');
+        socket.close();
+      }, sessionDuration + 3000);
     });
 
-    socket.on('ping', function () {
-      console.log('PING!');
-    });
-
-    socket.on('pong', function () {
-      console.log('PONG!');
-    });
-
-    socket.on('close', function () {
-      console.log(`VU ${__VU}: disconnected`);
-    });
-
-    socket.setTimeout(function () {
-      console.log('Closing the socket forcefully 3s after graceful LEAVE');
-      socket.close();
-    }, sessionDuration + 3000);
-  });
-
-  check(res, { 'Connected successfully': (r) => r && r.status === 101 });
+    check(res, {'Connected successfully': (r) => r && r.status === 101});
 }
 
