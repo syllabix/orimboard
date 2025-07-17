3d0ede413f23b3769e0d1d9e42628b1c77773017
Tom Stoepker <tom.stoepker@gmail.com>
2025-07-17 19:11:20 +0200

    chore: update style frameworks and config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,14 +3,5 @@ module.exports = {
         './pages/**/*.{js,ts,jsx,tsx}',
         './components/**/*.{js,ts,jsx,tsx}'
     ],
-    plugins: [require('daisyui')],
-    daisyui: {
-        styled: true,
-        themes: ["dracula"],
-        base: true,
-        utils: true,
-        logs: true,
-        rtl: false,
-        darkTheme: "dracula"
-    },
+    plugins: [],
 };
